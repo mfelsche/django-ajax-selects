6d8209216503fa7ed4cf5acd71f03306e97f9bba
matthias <matthias@itemfire.com>
2013-02-11 12:57:41 +0100

    changes for switch to jquery ui > 1.6

--- ajax_select/static/js/ajax_select.js
+++ ajax_select/static/js/ajax_select.js
@@ -5,7 +5,7 @@ if(typeof jQuery.fn.autocompletehtml != 'function') {
 
 $.fn.autocompletehtml = function() {
 	var $text = $(this), sizeul = true;
-	this.data("autocomplete")._renderItem = function _renderItemHTML(ul, item) {
+	this.data("uiAutocomplete")._renderItem = function _renderItemHTML(ul, item) {
 		if(sizeul) {
 			if(ul.css('max-width')=='none') ul.css('max-width',$text.outerWidth());
 			sizeul = false;
